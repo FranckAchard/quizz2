4668227e95ed8e06b23f89aed2fe59ad91f669c3
Utilisateur <Utilisateur@BNPPF006.home>
2018-02-15 16:52:54 +0100

    version stable

--- src/co/simplon/pf1/Quizz2.java
+++ src/co/simplon/pf1/Quizz2.java
@@ -7,29 +7,14 @@ public class Quizz2 {
 	/******************************************
 	 *  attributs
 	 *****************************************/
-	
-	// listes des questions
-	private static String[] questions= {
-			"Il est sympa Waby?",
-			"Combien il soulève?",
-			"Le PSG s'en sortira-t-il?"
-	};
-	
-	// liste des réponses
-	private static String[] answers= {
-			"Oui, trop sympa, je le kiffe vegra!!!",
-			"130 kg, la vache!",
-			"oui bien sûr!"
-	};
-	
-	// liste des scores a donner par reponse
-	private static int[] scores= {1, 2, 3};
 
-	// score a calculer	
+	// quizz elements
+	private static TestItem[] quizz= new TestItem[3];
+
+	// score total a calculer	
 	private static int score= 0;
-	
 
-	
+
 
 	/******************************************
 	 *  methodes 
@@ -37,30 +22,65 @@ public class Quizz2 {
 
 	// methode main
 	public static void main(String[] args) {
-		
-		/* boucle sur la liste de questions :
-		 * afficher la i eme question
-		 * récupérer la réponse
-		 * comparer avec la i eme réponse de la liste des réponses OK
-		 * si réponse OK, augmenter le score de la valeur du i eme score
-		 */
-		
+
+		// initialize quizz
+		quizz[0]= new TestItem("Il est sympa Waby?", "oui", 1);
+		quizz[1]= new TestItem("Combien il soulève en kg?", "130", 3);
+		quizz[2]= new TestItem("Le PSG s'en sortira-t-il?", "oui", 7);
+
 		// scanner pour récupérer saisie réponse
-		Scanner in= new Scanner(System.in);
+		Scanner input= new Scanner(System.in);
+
+		/*
+		for (int i=0; i<quizz.length; i++) {
+			
+			// print question
+			quizz[i].printQuestion();
+			
+			// input answer
+			String answer= input.nextLine();
+			
+			// if good answer, add points to score
+			if (quizz[i].rightAnswer(answer)) {
+				score+= quizz[i].getPoints();
+				System.out.println("Bonne réponse! le score augmente de " + quizz[i].getPoints() + " points et atteint un total temporaire de " + score);
+				System.out.println();
+			}
+			else {
+				System.out.println("Mauvaise réponse! le score temporaire reste à " + score + " points");
+				System.out.println();
+			}
+		}
+		*/
 		
-		for(int i=0; i != questions.length; ++i){
-		     System.out.println(questions[i]);
-		     String answer= in.nextLine();
-		     if(answer.equals(answers[i])){
-		         score+= scores[i];
-		     }
-		 }
-		 displayResult();
+		for (TestItem ti: quizz) {
+			
+			// print question
+			ti.printQuestion();
+			
+			// input answer
+			String answer= input.nextLine();
+			
+			// if good answer, add points to score
+			if (ti.rightAnswer(answer)) {
+				score+= ti.getPoints();
+				System.out.println("Bonne réponse! le score augmente de " + ti.getPoints() + " points et atteint un total temporaire de " + score);
+				System.out.println();
+			}
+			else {
+				System.out.println("Mauvaise réponse! le score temporaire reste à " + score + " points");
+				System.out.println();
+			}
+		}
+
+
+		displayResult();
+		input.close();
 	}
-	
+
 	// methode pour affichage des résultats
-	private static void displayResult( ) {
-		System.out.println("Le score total est " + score);
+	private static void displayResult() {
+		System.out.println("Le score final est " + score);
 	}
 
 }
---
+++ src/co/simplon/pf1/TestItem.java
@@ -0,0 +1,77 @@
+package co.simplon.pf1;
+
+public class TestItem {
+	// attibutes
+	private String question;
+	private String answer;
+	private int points;
+
+	// constructors
+	public TestItem() {
+		question="";
+		answer="";
+		points=0;
+	}
+	
+	public TestItem(String question, String answer, int points) {
+		super();
+		this.question = question;
+		this.answer = answer;
+		this.points = points;
+	}
+
+
+	// getters + setters
+	public String getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+
+	@Override
+	// toString
+	public String toString() {
+		return "TestItem [question=" + question + ", answer=" + answer + ", points=" + points + "]";
+	}
+
+	
+	// print attributes methods
+	public void printQuestion() {
+		System.out.println("question= " + this.question);
+	}
+	
+	public void printAnswer() {
+		System.out.println("answer= " + this.answer);
+	}
+	
+	public void printPoints() {
+		System.out.println("points= " + this.points);
+	}
+	
+	public boolean rightAnswer(String answer) {
+		boolean result=false;
+		if (this.answer.equals(answer)) {
+			result= true;
+		}
+		return result;
+	}
+}
